22f14a0b0948568b2c0dc2082bbf71da9487bf7f
David <ber.dev@yandex.ru>
2021-06-04 16:25:51 +0300

    Initial Commit

---
+++ StudySwiftUI2/StudySwiftUI2App.swift
@@ -0,0 +1,17 @@
+//
+//  StudySwiftUI2App.swift
+//  StudySwiftUI2
+//
+//  Created by Дэвид Бердников on 04.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct StudySwiftUI2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
